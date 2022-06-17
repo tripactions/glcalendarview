0f8b97e2caf9279b5921e7fd94e9d910faa69113
Alexander Fanaian <afanaian@eufrankfurt-1242427624.eu-central-1.elb.amazonaws.com>
2022-06-17 16:47:15 -0500

    Adding SPM support. There are no actual project changes. Just had to move folders around for sources / test to match what apple wants.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GLCalendarView",
+    dependencies: [
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .executableTarget(
+            name: "GLCalendarView",
+            dependencies: []),
+        .testTarget(
+            name: "GLCalendarViewTests",
+            dependencies: ["GLCalendarView"]),
+    ]
+)
